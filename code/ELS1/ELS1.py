d07b3248c8b6b1c7a2e540f0731a3b71ed49527c
Minhyun Yoo <ymh1989@korea.ac.kr>
2016-05-24 17:17:33 +0900

    One-asset ELS

--- code/ELS1/ELS1.py
+++ code/ELS1/ELS1.py
@@ -9,27 +9,25 @@ import numpy as np
 from math import exp, sqrt, ceil
 
 S0 = 100.0; # underlying price
-E = 100.0; # strike price
 T = 3.0; # maturity
-r = 0.0165; # riskless interest rate
-discr = 0.0165; # discount rate
+r = 0.03; # riskless interest rate
+discr = 0.03; # discount rate
 sig = 0.3; # volatility
 ns = 10000;  # # of simulations
 dateConv = 360; # 1 year
 nStep = int(dateConv * T); # # of time steps
 
-Kib = 60.0;
-cpy = 0.025;
-dummy = cpy*T;
-face = 10000.0;
-B = [95, 90, 85, 80, 75, 70];
+Kib = 60.0; # knock in barrier
+cpy = 0.03; # coupon per year
+dummy = cpy*T; # dummy
+face = 10000.0; # face value
+B = [90.0, 90.0, 85.0, 85.0, 80.0, 80.0]; # early redemption barrier
 numExercise = len(B);
 obsDate = np.array([ceil(nStep / numExercise), ceil(2.0*nStep/numExercise), ceil(3.0*nStep/numExercise),
-           ceil(4.0*nStep / numExercise), ceil(5.0*nStep / numExercise), ceil(nStep)]);
-payment = face*(1.0+np.array([cpy*0.5, cpy*1.0, cpy*1.5, cpy*2.0, cpy*2.5, cpy*3.0]));
+           ceil(4.0*nStep / numExercise), ceil(5.0*nStep / numExercise), ceil(nStep)]); # observation dates
+payment = face*(1.0+np.array([cpy*0.5, cpy*1.0, cpy*1.5, cpy*2.0, cpy*2.5, cpy*3.0])); # payment
 
-# functions call
-dt = T / nStep;
+dt = T / nStep; # time step
 t0 = time.clock();
 
 sumPayoff = 0.0;
@@ -38,21 +36,27 @@ idx = np.zeros(numExercise);
 drift = (r - 0.5*sig**2)*dt;
 sigsqdt = sig*sqrt(dt);
 
+# simulation loop
 for i in xrange(ns):
     s = S0; kievent = False; tag = False;
     cnt1 = 0; cnt2 = 0;
     payoff = 0.0; idx[:] = 0.0;
     
+    # random number
     z = np.random.normal(size = [nStep]);
+    # generating path
     for j in xrange(nStep):
         s = s * exp(drift + sigsqdt*z[j]);
         
+        # check hitting knock in barrier
         kievent = True if s < Kib else kievent;
         
+        # save the underlying price at observation dates
         if ((j+1) == obsDate[cnt1]):
             idx[cnt1] = s;
             cnt1 += 1;
             
+    # check for early redemption        
     for k in xrange(numExercise):
         if (idx[k] >= B[k]):
             payoff = payment[k];
@@ -61,13 +65,13 @@ for i in xrange(ns):
             break;
             
     if (tag == False):
-        payoff = 100.0 * s;
+        payoff = 100.0 * s; # loss
         if (kievent == False):
             if (s >= Kib):
-                payoff = face * (1.0 + dummy);
+                payoff = face * (1.0 + dummy); # dummy
     sumPayoff += payoff * exp(-discr * obsDate[cnt2] / dateConv);   
 
-    
+# mean
 els1 = sumPayoff / ns;
 t1 = time.clock();
 del z;
--- code/ELS1/ELS1_vec_test.py
+++ code/ELS1/ELS1_vec_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-European call option by Monte Carlo simulation 
+ELS 1-asset by Monte Carlo simulation 
 
 test for vectorized calculation
 
@@ -8,29 +8,28 @@ test for vectorized calculation
 """
 import time
 import numpy as np
-from math import exp, sqrt, ceil
+from math import sqrt, ceil
 
 S0 = 100.0; # underlying price
-E = 100.0; # strike price
 T = 3.0; # maturity
-r = 0.0165; # riskless interest rate
+r = 0.03; # riskless interest rate
+discr = 0.03; # discount rate
 sig = 0.3; # volatility
 ns = 10000;  # # of simulations
 dateConv = 360; # 1 year
 nStep = int(dateConv * T); # # of time steps
 
-Kib = 60.0;
-cpy = 0.025;
-dummy = cpy*T;
-face = 10000.0;
-B = [95, 90, 85, 80, 75, 70];
+Kib = 60.0; # knock in barrier
+cpy = 0.03; # coupon per year
+dummy = cpy*T; # dummy
+face = 10000.0; # face value
+B = [90.0, 90.0, 85.0, 85.0, 80.0, 80.0]; # early redemption barrier
 numExercise = len(B);
 obsDate = np.array([ceil(nStep / numExercise), ceil(2.0*nStep/numExercise), ceil(3.0*nStep/numExercise),
-           ceil(4.0*nStep / numExercise), ceil(5.0*nStep / numExercise), ceil(nStep)]);
-payment = face*(1.0+np.array([cpy*0.5, cpy*1.0, cpy*1.5, cpy*2.0, cpy*2.5, cpy*3.0]));
+           ceil(4.0*nStep / numExercise), ceil(5.0*nStep / numExercise), ceil(nStep)]); # observation dates
+payment = face*(1.0+np.array([cpy*0.5, cpy*1.0, cpy*1.5, cpy*2.0, cpy*2.5, cpy*3.0])); # payment
 
-# functions call
-dt = T / nStep;
+dt = T / nStep; # time step
 t0 = time.clock();
 
 sumPayoff = 0.0;
@@ -39,7 +38,7 @@ idx = np.zeros(numExercise);
 drift = (r - 0.5*sig**2)*dt;
 sigsqdt = sig*sqrt(dt);
 
-
+# vectorization
 s = S0 * np.ones([ns]); 
 kievent = np.zeros([ns], dtype = 'bool'); 
 tag = np.zeros([ns], dtype = 'bool'); 
@@ -48,49 +47,55 @@ cnt2 = -1.0*np.ones([ns]); # -1 : trick for 1st eary redemption
 payoff = np.zeros([ns]); 
 idx = np.zeros([ns, numExercise]);
 sp1 = np.zeros([ns, nStep]);
+
+# random number
 z = np.random.normal(size = [ns, nStep]);
+
+# generating time steps
 for i in xrange(nStep):
     s[:] = s[:] * np.exp(drift + sigsqdt*z[:, i]);
-    sp1[:, i] = s[:];
-    kievent = kievent | (s < Kib);
-      
+    
+    # check hitting knock in barrier by logical operator
+    kievent = kievent | (s < Kib); 
+    
+    # save the underlying price at observation dates
     if ((i+1) == obsDate[cnt1]):
         idx[:, cnt1] = s[:];
         cnt1 += 1;
-            
+
+# check for early redemption
 for j in xrange(numExercise):
-#    if (idx[j,:] >= B[j]):
-#        payoff = payment[j];
-#        tag = True;
-#        cnt2 = j;
-#        break;
+    #(bool set) chk1: aboce barrier & not exercised 
     chk1 = (idx[:,j] >= B[j]) & (cnt2==-1.0);
+    
+    # save index of observation dates at cnt2
     cnt2[chk1] = j;
+    
+    # determine payoff 
     payoff[chk1] = payment[j];
 
+#(bool set) chk2: not exercised & hit knock in barrier
 chk2 = (cnt2 == -1.0) & (kievent == True);
-payoff[chk2] = 100.0 * s[chk2]; # loss
 
+# determine payoff - loss
+payoff[chk2] = 100.0 * s[chk2]; 
+
+#(bool set) chk3: not exercised & not hit Kib & above Kib
 chk3 = (cnt2 == -1.0) & (kievent == False) & (s >= Kib);
+
+# determine payoff - dummy
 payoff[chk3] = face * (1.0 + dummy);
 
+# discount using index of observation dates
 cnt2[(cnt2 == -1.0)] = numExercise-1;
 cnt2 = cnt2.astype(int);
-payoff = payoff * np.exp(-r * obsDate[cnt2] / dateConv);
+payoff = payoff * np.exp(-discr * obsDate[cnt2] / dateConv);
+
+# mean
 els1 = np.mean(payoff);
-#els1 = mean()
-#if (tag == False):
-#    payoff = 100.0 * s;
-#    if (kievent == False):
-#        if (s >= Kib):
-#            payoff = face * (1.0 + dummy);
-#
-#sumPayoff += payoff * exp(-r * obsDate[cnt2] / dateConv);   
-#
-#    
-#els1 = sumPayoff / ns;
+
 t1 = time.clock();
 del z;
-#    
+
 print 'Monte Carlo Call Price : %.5f' % els1
 print 'CPU time in Python(sec) : %.4f' % (t1-t0)
